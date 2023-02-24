47f61ecf6e37f0ed9f62765c1fa457bb6e8d2e1f
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-02-24 23:30:04 +0200

    added home section and motion

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,12 @@
+import { SelectedPage } from "@/shared/types";
+import React from "react";
+
+type Props = {
+  setSelectedPage: (val: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return <div>Benefits</div>;
+};
+
+export default Benefits;
